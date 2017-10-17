b26d23604c72e8e784d1a8b461d66f3a6de04b59
unfairavacado <32792494+unfairavacado@users.noreply.github.com>
2017-10-17 15:04:40 +0530

    action.php

---
+++ action.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE html>
+<html>
+<style>
+body {
+    font-family: Verdana, Arial, sans-serif;
+    font-size: smaller;
+    padding: 50px;
+    color: #be7429;
+}
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 40px;
+}
+</style>
+<div>
+    <head>  
+    <body background="bgimg.png">         
+        <title>Response page</title>
+    </head>
+    <body>
+       <form action="add-questions.html">
+        <h1>Your response has been recorded.</h1>
+        <tr>
+            <td>
+                
+                Question:<?php $question=$_POST['question']; echo $question;?></br>
+                Option A:<?php $optiona=$_POST['optiona']; echo $optiona;?></br>
+                Option B:<?php $optionb=$_POST['optionb']; echo $optionb;?></br>
+                Option C:<?php $optionc=$_POST['optionc']; echo $optionc;?></br>
+                Option D:<?php $optiond=$_POST['optiond']; echo $optiond;?></br>
+                Key Option:<?php $keyoption=$_POST['keyoption']; echo $keyoption;?></br>
+                
+                <?php 
+$servername = "localhost";
+$username = "root";
+$password = "0000";
+$dbname = "questions";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error)
+{
+die("connection failed:".$conn->connect_error);
+}
+$sql = "INSERT INTO question (question, optiona, optionb, optionc, optiond, keyoption)
+VALUES ('$question', '$optiona', '$optionb','$optionc','$optiond','$keyoption')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Your response has been recorded successfully.";
+} else {
+    echo "Error in recording response: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
+
+                                          
+                        
+             <input type="submit" value="Add another" style="width: 99%">           
+            </td>
+        </tr>
+    </form>
+    </body>
+</div>
+    </html>
+
